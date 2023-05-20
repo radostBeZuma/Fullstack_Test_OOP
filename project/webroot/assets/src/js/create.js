ca2701c91d106a3dfa2ce3d84daae97994b8de8b
radostBeZuma <romahasasha@yandex.ru>
2023-05-20 14:29:03 +0300

    Сделал проверку в файле на существование элемента

--- project/webroot/assets/src/js/create.js
+++ project/webroot/assets/src/js/create.js
@@ -1,65 +1,66 @@
 const formInst = document.getElementById('addForm');
-const formSubmit = document.getElementById('btnAdd');
 
-const formField = {
-    title: document.getElementById('InputTitle'),
-    anounce: document.getElementById('TextareaAnounce'),
-    fileAnounce: document.getElementById('FileAnounceImg'),
-    detail: document.getElementById('TextareaDetail'),
-    fileDetail: document.getElementById('FileDetailImg'),
-}
-
-let errorCount;
-
-formInst.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    let data = new FormData();
-
-    data.append( 'title', formField.title.value);
-    data.append('anounce', formField.anounce.value);
-    data.append('fileAnounce', formField.fileAnounce.files[0]);
-    data.append('detail', formField.detail.value);
-    data.append('fileDetail', formField.fileDetail.files[0]);
-
-    sendNReceiveJson('/create/', 'POST', data);
-});
-
-function sendNReceiveJson(url, method, data) {
-    fetch(url,
-        {
-            method: method,
-            body: data
-        })
-        .then(function(res)
-        {return res.json();})
-        .then(function(data)
-        {
-           if(!data.ok) {
-               getError(document.querySelector('.all-error'), data.fields, errorCount);
-           }
-           else {
-               okMethod(data.id);
-           }
-        })
-}
-
-function getError(errTool, fields, err) {
-    const allError = errTool;
-
-    if (err > 0) {
-        allError.innerHTML = '';
-        err = 0;
+if (formInst) {
+    const formField = {
+        title: document.getElementById('InputTitle'),
+        anounce: document.getElementById('TextareaAnounce'),
+        fileAnounce: document.getElementById('FileAnounceImg'),
+        detail: document.getElementById('TextareaDetail'),
+        fileDetail: document.getElementById('FileDetailImg'),
     }
 
-    for (let key of Object.keys(fields)) {
-        allError.innerHTML += '<p>' + fields[key] + ' ' + key + '</p>';
+    let errorCount;
+
+    formInst.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let data = new FormData();
+
+        data.append( 'title', formField.title.value);
+        data.append('anounce', formField.anounce.value);
+        data.append('fileAnounce', formField.fileAnounce.files[0]);
+        data.append('detail', formField.detail.value);
+        data.append('fileDetail', formField.fileDetail.files[0]);
+
+        sendNReceiveJson('/create/', 'POST', data);
+    });
+
+    function sendNReceiveJson(url, method, data) {
+        fetch(url,
+            {
+                method: method,
+                body: data
+            })
+            .then(function(res)
+            {return res.json();})
+            .then(function(data)
+            {
+                if(!data.ok) {
+                    getError(document.querySelector('.all-error'), data.fields, errorCount);
+                }
+                else {
+                    okMethod(data.id);
+                }
+            })
     }
 
-    err++;
-}
+    function getError(errTool, fields, err) {
+        const allError = errTool;
 
-function okMethod(id) {
-    $newPage = '/' + id + '/';
-    window.location.replace($newPage);
+        if (err > 0) {
+            allError.innerHTML = '';
+            err = 0;
+        }
+
+        for (let key of Object.keys(fields)) {
+            allError.innerHTML += '<p>' + fields[key] + ' ' + key + '</p>';
+        }
+
+        err++;
+    }
+
+    function okMethod(id) {
+        $newPage = '/' + id + '/';
+        window.location.replace($newPage);
+    }
 }
