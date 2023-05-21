26357591e56a4183cc1fd698c1d3540f8f8b3231
radostBeZuma <romahasasha@yandex.ru>
2023-05-21 23:50:21 +0300

    Изменение layout для ошибки

--- project/Controllers/ErrorController.php
+++ project/Controllers/ErrorController.php
@@ -5,6 +5,11 @@ use Core\Base\Controller;
 class ErrorController extends Controller
 {
     public function notFound() {
+        $this->setLayout('error');
         return $this->render('Страница не найдена', 'error/notFound');
+    }
+
+    private function setLayout($name) {
+        return $this->layout = $name;
     }
 }
---
+++ project/layouts/error.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $title ?></title>
+
+    <link rel="icon" type="image/png" href="/favicon.png"/>
+
+    <link rel="stylesheet" href="/project/webroot/assets/build/css/style.min.css">
+</head>
+<body>
+<div class="container">
+    <h1 class="layout-title fs-4 fw-bold my-4 text-uppercase"><?= $title ?></h1>
+</div>
+
+<?= $content ?>
+
+<script src="/project/webroot/assets/build/js/main.min.js"></script>
+</body>
+</html>
