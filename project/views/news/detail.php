2e39cb415425355657defde74a78d5e962a4b12d
radostBeZuma <romahasasha@yandex.ru>
2023-05-22 18:54:14 +0300

    Изменение отображение детальной новости

--- project/views/news/detail.php
+++ project/views/news/detail.php
@@ -4,10 +4,10 @@
             <h2 class="detail__title"><?= $oneNews['title'] ?></h2>
             <div class="detail__date mb-3"><?= $oneNews['date_created'] ?></div>
             <button class="btn btn-warning mb-3 d-flex align-items-center" title="Редактировать" type="button" data-bs-toggle="modal" data-bs-target="#updateModal" data-bs-id="<?= $oneNews['id'] ?>"">
-                <img src="/project/webroot/assets/build/img/update.png" alt="" width="15" height="15">
+                <img src="/project/webroot/assets/build/img/update.png" alt="Редактировать" width="15" height="15">
                 <span class="text-white ms-2">Редактировать</span>
             </button>
-            <img class="detail__img float-start pb-3 pe-3" src="<?= $oneNews['detail_url'] ?>" alt="">
+            <img class="detail__img float-none float-sm-start mb-3 pe-0 pe-sm-3" src="<?= $oneNews['detail_url'] ?>" alt="<?= $oneNews['title'] ?>" title="<?= $oneNews['title'] ?>">
             <div class="detail__text"><?= $oneNews['detail_text'] ?></div>
         </div>
         <div class="detail__backurl mb-4">
